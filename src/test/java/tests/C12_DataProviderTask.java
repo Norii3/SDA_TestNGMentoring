ef599eae226b6839d2d1ae3114b6a6aa0000b62f
Norii3 <norahksh33@gmail.com>
2025-09-28 12:12:34 +0300

    1

--- src/test/java/tests/C12_DataProviderTask.java
+++ src/test/java/tests/C12_DataProviderTask.java
@@ -1,61 +1,57 @@
 package tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class C12_DataProviderTask extends TestBase {
+    /*
+    Go to https://claruswaysda.github.io/Registration.html
+    Fill the form
+    Do it with all 3 ways
+    (Same class Data Provider, Data Provider with Faker, Data Provider with Excel)
+    */
 
-    @Test(dataProvider = "getFormData", dataProviderClass = utilities.DataProviderUtilities.class )
-    void dataProviderTask(String ssn,String firstName, String lastName,String job, String username, String email, String password){
+    @Test(dataProvider = "form")
+    void dataProviderTaskTest01(String ssn, String firstname, String lastname) {
+//        Go to https://claruswaysda.github.io/Registration.html
         driver.get("https://claruswaysda.github.io/Registration.html");
+//        Fill the form
         driver.findElement(By.id("ssn")).sendKeys(ssn);
-        driver.findElement(By.id("firstName")).sendKeys(firstName);
-        driver.findElement(By.id("lastName")).sendKeys(lastName);
-        //gender
-        WebElement dd = driver.findElement(By.id("job"));
-        new Select(dd).selectByVisibleText(job);
-        //pdf
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("email")).sendKeys(email);
-        driver.findElement(By.id("password")).sendKeys(password);
-
+        driver.findElement(By.id("firstName")).sendKeys(firstname);
+        driver.findElement(By.id("lastName")).sendKeys(lastname);
     }
 
-    @Test(dataProvider = "faker", dataProviderClass = utilities.DataProviderUtilities.class )
-    void dPTaskFaker2(String ssn,String firstName, String lastName,String job, String username, String email, String password){
+    @Test(dataProvider = "faker", dataProviderClass = utilities.DataProviderUtilities.class)
+    void dataProviderTaskTest02(String ssn, String firstname, String lastname) {
+//        Go to https://claruswaysda.github.io/Registration.html
         driver.get("https://claruswaysda.github.io/Registration.html");
+//        Fill the form
         driver.findElement(By.id("ssn")).sendKeys(ssn);
-        driver.findElement(By.id("firstName")).sendKeys(firstName);
-        driver.findElement(By.id("lastName")).sendKeys(lastName);
-        //gender
-        WebElement dd = driver.findElement(By.id("job"));
-        new Select(dd).selectByVisibleText(job);
-        //pdf
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("email")).sendKeys(email);
-        driver.findElement(By.id("password")).sendKeys(password);
-
+        driver.findElement(By.id("firstName")).sendKeys(firstname);
+        driver.findElement(By.id("lastName")).sendKeys(lastname);
     }
 
-    //excel
-
-    @Test(dataProvider = "excelDP", dataProviderClass = utilities.DataProviderUtilities.class )
-        void dPTaskExcel3(String ssn,String firstName, String lastName,String job, String username, String email, String password){
+    @Test(dataProvider = "excel", dataProviderClass = utilities.DataProviderUtilities.class)
+    void dataProviderTaskTest03(String ssn, String firstname, String lastname) {
+//        Go to https://claruswaysda.github.io/Registration.html
         driver.get("https://claruswaysda.github.io/Registration.html");
+//        Fill the form
         driver.findElement(By.id("ssn")).sendKeys(ssn);
-        driver.findElement(By.id("firstName")).sendKeys(firstName);
-        driver.findElement(By.id("lastName")).sendKeys(lastName);
-        //gender
-        WebElement dd = driver.findElement(By.id("job"));
-        new Select(dd).selectByVisibleText(job);
-        //pdf
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("email")).sendKeys(email);
-        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("firstName")).sendKeys(firstname);
+        driver.findElement(By.id("lastName")).sendKeys(lastname);
+    }
 
+
+
+    @DataProvider(name = "form")
+    public Object[][] getFormData() {
+        return new Object[][]{
+                {"123-66-4567", "John", "Doe"},
+                {"654-66-9876", "Tom", "Stark"},
+                {"456-55-8799", "Wayne", "Krank"}
+        };
     }
 
 }
